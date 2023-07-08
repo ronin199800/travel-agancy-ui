b0ef9b5216836bdd9029775f6fe9d3287f5c5668
mohammadRezaMashhadi <rezam26667.mr@gamil.com>
2023-07-08 15:42:59 +0330

    each-article

--- src/routes/eachArticle.jsx
+++ src/routes/eachArticle.jsx
@@ -1,9 +1,23 @@
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const EachArticle = () => {
+  const { id } = useParams();
+  const [eachArticle, setEachArticle] = useState({});
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `http://localhost:5000/api/article/${id}`
+      );
+      setEachArticle(response.data.data);
+    };
 
-  return <h1 style={{ color: 'red' }}>helooo</h1>;
-}
+    fetchData();
+  }, []);
+
+  return <div>{eachArticle._id}</div>;
+};
 
 export default EachArticle;
