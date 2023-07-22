1e55933f77c5a491e848f4c3b8d73b2be03d1072
mohammadRezaMashhadi <rezam26667.mr@gamil.com>
2023-07-22 14:10:26 +0330

    remove dead file

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import EachArticle from "./routes/eachArticle";
 import Panel from "./components/panel/panel";
 import PanelNav from "./components/panel/panelNav";
 import PCategory from "./components/panel/article/category";
-import PostArticleCat from "./components/panel/postArticle/postArticleCat";
 
 function App() {
   const [mode, setmode] = useState("light");
@@ -56,7 +55,6 @@ function App() {
           <Route path="/panel/articles" element={<PArticle />} />
           <Route path="/panel/articles/post" element={<PostArticle />} />
           <Route path="/panel/articles/category" element={<PCategory />} />
-          <Route path="/panel/articles/category/post" element={<PostArticleCat />} />
           <Route path="/not-found" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Navigate to="/not-found" />} />
